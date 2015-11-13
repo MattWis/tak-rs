8ec1f59f6c163cd55ff1b80183b16b08f9c888dd
Matt Wismer <mattwis86@gmail.com>
2015-11-12 20:33:07 -0500

    Test module

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ pub mod point;
 pub mod turn;
 pub mod board;
 
-use turn::Turn;
+pub use turn::Turn;
 
-fn play(turn: &Turn, board: &mut board::Board) -> () {
+pub fn play(turn: &Turn, board: &mut board::Board) -> () {
     match turn {
         &Turn::Placement { ref point, ref piece } => {
             board.at(point).place_piece(*piece);
@@ -28,102 +28,107 @@ fn play(turn: &Turn, board: &mut board::Board) -> () {
     }
 }
 
-#[test]
-fn basic_placement() {
-    let mut game = board::Board::new(4);
-    play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
-    play(&("d3C2".parse::<Turn>().unwrap()), &mut game);
-    println!("{}", game);
-    assert_eq!(game.to_string(), "____________\n\
-                                  |  |  |  |  \n\
-                                  |  |  |  |C2\n\
-                                  |F2|  |  |  \n\
-                                  |S1|  |  |  \n");
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn basic_movement() {
-    let mut game = board::Board::new(4);
-    play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
-    play(&("a1U1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2R12".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.to_string(), "____________\n\
-                                  |  |  |  |  \n\
-                                  |  |  |  |  \n\
-                                  |  |F2|S1|  \n\
-                                  |  |  |  |  \n");
-}
+    #[test]
+    fn basic_placement() {
+        let mut game = board::Board::new(4);
+        play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
+        play(&("d3C2".parse::<Turn>().unwrap()), &mut game);
+        println!("{}", game);
+        assert_eq!(game.to_string(), "____________\n\
+                                      |  |  |  |  \n\
+                                      |  |  |  |C2\n\
+                                      |F2|  |  |  \n\
+                                      |S1|  |  |  \n");
+    }
 
-#[test]
-#[should_panic]
-fn invalid_movement_onto_standing() {
-    let mut game = board::Board::new(4);
-    play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
-}
+    #[test]
+    fn basic_movement() {
+        let mut game = board::Board::new(4);
+        play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
+        play(&("a1U1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2R12".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.to_string(), "____________\n\
+                                      |  |  |  |  \n\
+                                      |  |  |  |  \n\
+                                      |  |F2|S1|  \n\
+                                      |  |  |  |  \n");
+    }
 
-#[test]
-#[should_panic]
-fn invalid_movement_onto_capstone() {
-    let mut game = board::Board::new(4);
-    play(&("a1C1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
-}
+    #[test]
+    #[should_panic]
+    fn invalid_movement_onto_standing() {
+        let mut game = board::Board::new(4);
+        play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
+    }
 
-#[test]
-fn squash() {
-    let mut game = board::Board::new(4);
-    play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2C2".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.to_string(), "____________________\n\
-                                  |    |    |    |    \n\
-                                  |    |    |    |    \n\
-                                  |    |    |    |    \n\
-                                  |F1C2|    |    |    \n");
-}
+    #[test]
+    #[should_panic]
+    fn invalid_movement_onto_capstone() {
+        let mut game = board::Board::new(4);
+        play(&("a1C1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2F2".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
+    }
 
-#[test]
-fn win_across() {
-    let mut game = board::Board::new(4);
-    play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a2C1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a3F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("a4F1".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.check_winner(), Some(piece::Player::One));
-}
+    #[test]
+    fn squash() {
+        let mut game = board::Board::new(4);
+        play(&("a1S1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2C2".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2D1".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.to_string(), "____________________\n\
+                                      |    |    |    |    \n\
+                                      |    |    |    |    \n\
+                                      |    |    |    |    \n\
+                                      |F1C2|    |    |    \n");
+    }
 
-#[test]
-fn almost() {
-    let mut game = board::Board::new(4);
-    play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.check_winner(), None);
-}
+    #[test]
+    fn win_across() {
+        let mut game = board::Board::new(4);
+        play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a2C1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a3F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("a4F1".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.check_winner(), Some(piece::Player::One));
+    }
 
-#[test]
-fn win_up() {
-    let mut game = board::Board::new(4);
-    play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("d1C1".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.check_winner(), Some(piece::Player::One));
-}
+    #[test]
+    fn almost() {
+        let mut game = board::Board::new(4);
+        play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.check_winner(), None);
+    }
 
-#[test]
-fn cant_win_with_standing() {
-    let mut game = board::Board::new(4);
-    play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
-    play(&("d1S1".parse::<Turn>().unwrap()), &mut game);
-    assert_eq!(game.check_winner(), None);
+    #[test]
+    fn win_up() {
+        let mut game = board::Board::new(4);
+        play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("d1C1".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.check_winner(), Some(piece::Player::One));
+    }
+
+    #[test]
+    fn cant_win_with_standing() {
+        let mut game = board::Board::new(4);
+        play(&("a1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("b1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("c1F1".parse::<Turn>().unwrap()), &mut game);
+        play(&("d1S1".parse::<Turn>().unwrap()), &mut game);
+        assert_eq!(game.check_winner(), None);
+    }
 }
 
 fn main () {
